5afd95558c1ab6545d0ee98dbb77c0b76e87a809
DanielMwandiki <63342388+DanielMwandiki@users.noreply.github.com>
2024-04-11 04:25:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World! Fine morning. Trying ...... '); //write a response to the client
+  res.write('Hello World! Fine morning. Trying ...... Done'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 4000
